ea39d5d6987c8e87f51f0e2b893573c7b713ab42
rajnishQuadb <rajnish.tripathi@quadbtech.com>
2025-05-08 20:20:13 +0530

    refactor: update CORS configuration to restrict allowed origins and comment out referral redirect logic

--- routes/referralRoutes.ts
+++ routes/referralRoutes.ts
@@ -11,18 +11,27 @@ router.get('/get_referral_stats/:userId', referralController.getReferralStats as
 router.post('/join_referral', referralController.useReferralCode as RequestHandler);
 
 // Handle referral redirect
-router.get('/redirect/:referralCode', (req, res) => {
-  const { referralCode } = req.params;
+// router.get('/redirect/:referralCode', (req, res) => {
+//   const { referralCode } = req.params;
 
-  // Store referral code in a cookie that expires in 7 days
-  res.cookie('referral_code', referralCode, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax'
-  });
-  // Redirect to main page
-  res.redirect(`${process.env.NEXT_PUBLIC_API_BASE_URL_FRONTEND}`);
-});
+//   console.log(`Referral code: ${referralCode}`);
+
+//   // Store referral code in a cookie that expires in 7 days
+//   // res.cookie('referral_code', referralCode, {
+//   //   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+//   //   httpOnly: true,
+//   //   secure: process.env.NODE_ENV === 'production',
+//   //   sameSite: 'lax'
+//   // });
+
+//   // Redirect to main page
+//   // res.redirect(`https://clans-landing.10on10studios.com/referral/${referralCode}`);
+//   res.redirect(`https://clans-landing.10on10studios.com`);
+//   // res.send({
+//   //   message: 'Referral code stored in cookie',
+//   //   referralCode: referralCode,
+//   //   redirectUrl: `${process.env.NEXT_PUBLIC_API_BASE_URL_FRONTEND}`
+//   // })
+// });
 
 export default router;
